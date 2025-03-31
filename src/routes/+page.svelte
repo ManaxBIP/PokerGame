a8a04c74c5f4393813eb66b31807b4afe69e4091
NayanMallet <nayanmallet@gmail.com>
2025-03-31 16:10:25 +0200

    :rocket:feature(trcp): set up trcp sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
     import '../app.css';
     import HomeLayout from "$lib/layouts/home.svelte";
+
+    import { page } from '$app/stores';
+    import { trpc } from '$lib/trpc/client';
+
+    let greeting = 'press the button to load data';
+    let loading = false;
+
+    const loadData = async () => {
+        loading = true;
+        greeting = await trpc($page).greeting.query();
+        loading = false;
+    };
 </script>
 
 <HomeLayout>
     <playing-card suit=Hearts rank=10 class="w-20 h-20"></playing-card>
+    <a
+      href="#load"
+      role="button"
+      class="secondary"
+      aria-busy={loading}
+      on:click|preventDefault={loadData}>Load</a
+    >
+    <p>{greeting}</p>
 </HomeLayout>
