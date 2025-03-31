aed012333ce989d23f049fc4b48b1f283dfede09
NayanMallet <nayanmallet@gmail.com>
2025-03-31 14:52:28 +0200

    remove front

--- front/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import '../app.css';
-    import HomeLayout from "$lib/layouts/home.svelte";
-</script>
-
-<HomeLayout>
-    <playing-card suit=Hearts rank=10 class="w-20 h-20"></playing-card>
-</HomeLayout>
