f5e354de968852ecfa1a147ea859aab2a32b7b37
NayanMallet <nayanmallet@gmail.com>
2025-04-22 14:12:29 +0200

    ✨ Enhance sidebar with user authentication and dynamic avatar display

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,24 @@
-import { createContext } from '@/trpc/context';
-import { router } from '@/trpc/router';
 import type { Handle } from '@sveltejs/kit';
-import { createTRPCHandle } from 'trpc-sveltekit';
+import { JWT_SECRET } from '$env/static/private';
+import { prisma } from '$lib/prisma';
+import jwt from 'jsonwebtoken';
 
-export const handle: Handle = createTRPCHandle({ router, createContext });
+export const handle: Handle = async ({ event, resolve }) => {
+	const token = event.cookies.get('jwt');
+
+	if (token) {
+		try {
+			const decoded = jwt.verify(token, JWT_SECRET) as { id: number };
+			const user = await prisma.user.findUnique({ where: { id: decoded.id } });
+
+			if (user) {
+				event.locals.user = user;
+			}
+		} catch (err) {
+			console.error('Invalid JWT:', err);
+			event.cookies.delete('jwt', { path: '/' });
+		}
+	}
+
+	return resolve(event);
+};
