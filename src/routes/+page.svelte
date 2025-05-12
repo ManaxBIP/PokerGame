71e478bf23223a234d8460c1748233a8396e69f8
NotGrafik <pablo.jeanlouis@ynov.com>
2025-05-12 09:50:46 +0200

    ✨ Update landing page by removing carousel component and adding a new button for drawing cards in the playground

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,26 +67,6 @@
           </li>
         </ul>
       </div>
-      <Carousel.Root class="w-full max-w-xs animate-fade-in-up delay-[400ms]">
-        <h1 class="text-center text-md md:text-xl">Try it <strong>now</strong> !</h1>
-        <Carousel.Content>
-          {#each Array(5) as _, i (i)}
-            <Carousel.Item>
-              <div class="p-1">
-                <Card.Root>
-                  <Card.Content
-                    class="flex aspect-square items-center justify-center p-6"
-                  >
-                    <span class="text-4xl font-semibold">{i + 1}</span>
-                  </Card.Content>
-                </Card.Root>
-              </div>
-            </Carousel.Item>
-          {/each}
-        </Carousel.Content>
-        <Carousel.Previous />
-        <Carousel.Next />
-      </Carousel.Root>
 
       <Button href="/sign-up" class="mt-4" size="lg">Join us !</Button>
     </div>
